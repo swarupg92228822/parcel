72906bc6313abe5ecf0c49fddca55aef6e378c88
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2021-12-07 21:15:48 +0100

    REPL hacks

--- packages/core/core/src/requests/PathRequest.js
+++ packages/core/core/src/requests/PathRequest.js
@@ -143,7 +143,9 @@ export class ResolverRunner {
       diagnostic.codeFrames = [
         {
           filePath,
-          code: await this.options.inputFS.readFile(filePath, 'utf8'),
+          code: await this.options.inputFS
+            .readFile(filePath, 'utf8')
+            .catch(() => ''),
           codeHighlights: dependency.loc
             ? [{start: dependency.loc.start, end: dependency.loc.end}]
             : [],
